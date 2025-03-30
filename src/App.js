b3c01054a480dfff16f90991dca84f3ff543224b
PVSCH <srichaitanyapasupula2004@gmail.com>
2025-03-30 18:04:39 +0530

    added loading state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Navbar2 from './components/Navbar2';
-import HomePage from './components/HomePage';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Navbar2 from "./components/Navbar2";
+import HomePage from "./components/HomePage";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <div>
       <Navbar />
-      <Navbar2/>
-      <HomePage/>
-      
+      <Navbar2 />
+      {loading ? (
+        <div className="loading-container">
+          <div className="loading-spinner"></div>
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <HomePage />
+      )}
     </div>
   );
 }
