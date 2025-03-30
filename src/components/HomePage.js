eddef716626b2c808eb6155514fe95a7703501d5
PVSCH <srichaitanyapasupula2004@gmail.com>
2025-03-30 12:38:36 +0530

    1st info box completed

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,63 +1,75 @@
 import "./HomePage.css";
 
-
-const HomePage = ()=>{
-
-
-    return (<div class="home-1">
-       
-        <div class="home-2">
-             {/* Search Section */}
-             <div className="search-container">
-                <span>Also try searching for:</span>
-                <button className="search-btn">nike*</button>
-                <button className="search-btn">*ike</button>
-            </div>
-
-            {/* Table Layout */}
-            <div className="table-container">
-                <div className="table-head">
-                    <span>Mark</span>
-                    <span>Details</span>
-                    <span>Status</span>
-                    <span>Class/Description</span>
-                </div>
-
-                <div className="table-content">
-                    {/* Logo Section */}
-                    <div className="logo-box">
-                        <img src="nike-logo.png" alt="Nike Logo" />
-                    </div>
-
-                    {/* Details Section */}
-                    <div className="info-box">
-                        <strong>NIKE</strong>
-                        <span>Nike, Inc.</span>
-                        <span><b>73302505</b></span>
-                        <span>23 Mar 1981</span>
-                    </div>
-
-                    {/* Status Section */}
-                    <div className="status-box">
-                        <span className="active-status">● Live/Registered</span>
-                        <span>on 24 Oct 2023</span>
-                        <br />
-                        <span className="expire-status">🔄 21 Jun 2033</span>
-                    </div>
-
-                    {/* Class & Description Section */}
-                    <div className="class-box">
-                        <span>Retail Footwear and Apparel Store Services</span>
-                        <span>⚖ Class 042</span>
-                    </div>
-                </div>
-            </div>
+const HomePage = () => {
+  return (
+    <div class="home-1">
+      <div class="home-2">
+        <div class="tbl-1">
+          <div class="tbl-1-1">Also try searching for:</div>
+          <div class="tbl-1-2">
+            <button>nike*</button>
+          </div>
+          <div class="tbl-1-3">
+            <button>*ike</button>
+          </div>
         </div>
-        <div class="home-3"><h5>hi</h5></div>
-
-
-    </div>);
+        <div class="tbl-2">
+          <div class="tbl-2-1">Mark</div>
+          <div class="tbl-2-2">Details</div>
+          <div class="tbl-2-3">Status</div>
+          <div class="tbl-2-4">Class/Description</div>
+        </div>
+        <div class="tbl-3">
+          <div class="tbl-3-1">
+            <div class="tbl-3-1-1">
+              <img 
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTma5dtDVMb2B6TtUfZNa1-HyfPWJ803wotPg&s" 
+                alt="Trademark Image" 
+                style={{ width: "130px", height: "100px" }} /* Set image size */
+              />
+            </div>
+          </div>
+          <div class="tbl-3-2">
+            <div class="tbl-3-2-1">
+              <div class="tbl-3-2-1-1">
+                <div class="info-1">
+                  Nike
+                </div>
+                <div class="info-2">Nike, Inc.</div>
+              </div>
+              <div class="tbl-3-2-1-2">
+                <div class="info-3">
+                  <span class="green-dot"></span> 
+                  <span class="green-text">Live/Registered</span>
+                </div>
+                <div class="info-4">on 24th oct 2023</div>
+                </div>   
+              <div class="tbl-3-2-1-3">Retail footwear and service</div>
+            </div>
+            <div class="tbl-3-2-2">
+              <div class="tbl-3-2-2-1">
+                <div class="info-5">78905678</div>
+                <div class="info-6">23 Mar 1981</div>
+              </div>
+              <div class="tbl-3-2-2-2">
+                <img 
+                  src="https://w7.pngwing.com/pngs/498/480/png-transparent-arrow-circle-circular-text-symbol-smile-thumbnail.png" 
+                  alt="Arrow Icon" 
+                  style={{ width: "20px", height: "20px", marginRight: "8px" }} /* Set image size and spacing */
+                />
+                21st jun 2023
+              </div>
+              <div class="tbl-3-2-2-2">Class 042</div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      <div class="home-3">
+        <h5>hihi</h5>
+      </div>
+    </div>
+  );
 }
-
 
 export default HomePage;
