6bf5282d42405a01cd77d11937234524d55de721
PVSCH <srichaitanyapasupula2004@gmail.com>
2025-03-30 17:52:50 +0530

    css completion

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -73,6 +73,222 @@ const HomePage = () => {
             
           </div>
         </div>
+        <div class="tbl-3">
+          <div class="tbl-3-1">
+            <div class="tbl-3-1-1">
+              <img 
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTma5dtDVMb2B6TtUfZNa1-HyfPWJ803wotPg&s" 
+                alt="Trademark Image" 
+                style={{ width: "130px", height: "100px" }} /* Set image size */
+              />
+            </div>
+          </div>
+          <div class="tbl-3-2">
+            <div class="tbl-3-2-1">
+              <div class="tbl-3-2-1-1">
+                <div class="info-1">
+                  Nike
+                </div>
+                <div class="info-2">Nike, Inc.</div>
+              </div>
+              <div class="tbl-3-2-1-2">
+                <div class="info-3">
+                  <span class="green-dot"></span> 
+                  <span class="green-text">Live/Registered</span>
+                </div>
+                <div class="info-4">
+                  on <span class="bold-text">24th oct 2023</span>
+                </div>
+              </div>   
+              <div class="tbl-3-2-1-3">Retail footwear and service</div>
+            </div>
+            <div class="tbl-3-2-2">
+              <div class="tbl-3-2-2-1">
+                <div class="info-5">78905678</div>
+                <div class="info-6">23 Mar 1981</div>
+              </div>
+              <div class="tbl-3-2-2-2">
+                <img 
+                  src="https://w7.pngwing.com/pngs/498/480/png-transparent-arrow-circle-circular-text-symbol-smile-thumbnail.png" 
+                  alt="Arrow Icon" 
+                  style={{ width: "16px", height: "16px", marginRight: "5px" }} /* Decrease image size */
+                />
+                <span class="bold-small-text">21st jun 2023</span>
+              </div>
+              <div class="tbl-3-2-2-2">
+                <img 
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlGmKtrnxElpqw3AExKXPWWBulcwjlvDJa1Q&s" 
+                  alt="Class Icon" 
+                  style={{ width: "16px", height: "16px", marginRight: "5px" }} /* Decrease image size */
+                />
+                <span class="small-text">Class 042</span> 
+              </div>
+            </div>
+            
+          </div>
+        </div>
+        <div class="tbl-3">
+          <div class="tbl-3-1">
+            <div class="tbl-3-1-1">
+              <img 
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTma5dtDVMb2B6TtUfZNa1-HyfPWJ803wotPg&s" 
+                alt="Trademark Image" 
+                style={{ width: "130px", height: "100px" }} /* Set image size */
+              />
+            </div>
+          </div>
+          <div class="tbl-3-2">
+            <div class="tbl-3-2-1">
+              <div class="tbl-3-2-1-1">
+                <div class="info-1">
+                  Nike
+                </div>
+                <div class="info-2">Nike, Inc.</div>
+              </div>
+              <div class="tbl-3-2-1-2">
+                <div class="info-3">
+                  <span class="green-dot"></span> 
+                  <span class="green-text">Live/Registered</span>
+                </div>
+                <div class="info-4">
+                  on <span class="bold-text">24th oct 2023</span>
+                </div>
+              </div>   
+              <div class="tbl-3-2-1-3">Retail footwear and service</div>
+            </div>
+            <div class="tbl-3-2-2">
+              <div class="tbl-3-2-2-1">
+                <div class="info-5">78905678</div>
+                <div class="info-6">23 Mar 1981</div>
+              </div>
+              <div class="tbl-3-2-2-2">
+                <img 
+                  src="https://w7.pngwing.com/pngs/498/480/png-transparent-arrow-circle-circular-text-symbol-smile-thumbnail.png" 
+                  alt="Arrow Icon" 
+                  style={{ width: "16px", height: "16px", marginRight: "5px" }} /* Decrease image size */
+                />
+                <span class="bold-small-text">21st jun 2023</span>
+              </div>
+              <div class="tbl-3-2-2-2">
+                <img 
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlGmKtrnxElpqw3AExKXPWWBulcwjlvDJa1Q&s" 
+                  alt="Class Icon" 
+                  style={{ width: "16px", height: "16px", marginRight: "5px" }} /* Decrease image size */
+                />
+                <span class="small-text">Class 042</span> 
+              </div>
+            </div>
+            
+          </div>
+        </div>
+        <div class="tbl-3">
+          <div class="tbl-3-1">
+            <div class="tbl-3-1-1">
+              <img 
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTma5dtDVMb2B6TtUfZNa1-HyfPWJ803wotPg&s" 
+                alt="Trademark Image" 
+                style={{ width: "130px", height: "100px" }} /* Set image size */
+              />
+            </div>
+          </div>
+          <div class="tbl-3-2">
+            <div class="tbl-3-2-1">
+              <div class="tbl-3-2-1-1">
+                <div class="info-1">
+                  Nike
+                </div>
+                <div class="info-2">Nike, Inc.</div>
+              </div>
+              <div class="tbl-3-2-1-2">
+                <div class="info-3">
+                  <span class="green-dot"></span> 
+                  <span class="green-text">Live/Registered</span>
+                </div>
+                <div class="info-4">
+                  on <span class="bold-text">24th oct 2023</span>
+                </div>
+              </div>   
+              <div class="tbl-3-2-1-3">Retail footwear and service</div>
+            </div>
+            <div class="tbl-3-2-2">
+              <div class="tbl-3-2-2-1">
+                <div class="info-5">78905678</div>
+                <div class="info-6">23 Mar 1981</div>
+              </div>
+              <div class="tbl-3-2-2-2">
+                <img 
+                  src="https://w7.pngwing.com/pngs/498/480/png-transparent-arrow-circle-circular-text-symbol-smile-thumbnail.png" 
+                  alt="Arrow Icon" 
+                  style={{ width: "16px", height: "16px", marginRight: "5px" }} /* Decrease image size */
+                />
+                <span class="bold-small-text">21st jun 2023</span>
+              </div>
+              <div class="tbl-3-2-2-2">
+                <img 
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlGmKtrnxElpqw3AExKXPWWBulcwjlvDJa1Q&s" 
+                  alt="Class Icon" 
+                  style={{ width: "16px", height: "16px", marginRight: "5px" }} /* Decrease image size */
+                />
+                <span class="small-text">Class 042</span> 
+              </div>
+            </div>
+            
+          </div>
+        </div>
+        <div class="tbl-3">
+          <div class="tbl-3-1">
+            <div class="tbl-3-1-1">
+              <img 
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTma5dtDVMb2B6TtUfZNa1-HyfPWJ803wotPg&s" 
+                alt="Trademark Image" 
+                style={{ width: "130px", height: "100px" }} /* Set image size */
+              />
+            </div>
+          </div>
+          <div class="tbl-3-2">
+            <div class="tbl-3-2-1">
+              <div class="tbl-3-2-1-1">
+                <div class="info-1">
+                  Nike
+                </div>
+                <div class="info-2">Nike, Inc.</div>
+              </div>
+              <div class="tbl-3-2-1-2">
+                <div class="info-3">
+                  <span class="green-dot"></span> 
+                  <span class="green-text">Live/Registered</span>
+                </div>
+                <div class="info-4">
+                  on <span class="bold-text">24th oct 2023</span>
+                </div>
+              </div>   
+              <div class="tbl-3-2-1-3">Retail footwear and service</div>
+            </div>
+            <div class="tbl-3-2-2">
+              <div class="tbl-3-2-2-1">
+                <div class="info-5">78905678</div>
+                <div class="info-6">23 Mar 1981</div>
+              </div>
+              <div class="tbl-3-2-2-2">
+                <img 
+                  src="https://w7.pngwing.com/pngs/498/480/png-transparent-arrow-circle-circular-text-symbol-smile-thumbnail.png" 
+                  alt="Arrow Icon" 
+                  style={{ width: "16px", height: "16px", marginRight: "5px" }} /* Decrease image size */
+                />
+                <span class="bold-small-text">21st jun 2023</span>
+              </div>
+              <div class="tbl-3-2-2-2">
+                <img 
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlGmKtrnxElpqw3AExKXPWWBulcwjlvDJa1Q&s" 
+                  alt="Class Icon" 
+                  style={{ width: "16px", height: "16px", marginRight: "5px" }} /* Decrease image size */
+                />
+                <span class="small-text">Class 042</span> 
+              </div>
+            </div>
+            
+          </div>
+        </div>
       </div>
       <div class="home-3">
         
@@ -120,7 +336,25 @@ const HomePage = () => {
           <div class="box-10">List View</div>
           </div>
         </div>
-        
+        <div>
+
+        </div>
+        <div class="hidden-box">
+
+        <div class="hidden">hi</div>
+        <div class="hidden">hi</div>
+        <div class="hidden">hi</div>
+        <div class="hidden">hi</div>
+        <div class="hidden">hi</div>
+        <div class="hidden">hi</div>
+        <div class="hidden">hi</div>
+        <div class="hidden">hi</div>
+        <div class="hidden">hi</div>
+        <div class="hidden">hi</div>
+        <div class="hidden">hi</div>
+        <div class="hidden">hi</div>
+        <div class="hidden">hi</div>
+        </div>
         
       </div>
     </div>
